a59007ba660b25aab514444ca878c4dc9b8cf9ca
akhil1975 <77316648+akhil1975@users.noreply.github.com>
2025-09-21 13:17:24 +0100

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,5 +6,5 @@ def newMaven()
 {
   sh '''export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
             export PATH=$JAVA_HOME/bin:$PATH
-            mvn clean install
+            mvn clean install'''
 }
