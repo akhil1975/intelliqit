db778b704206b4d24de21c608f6b55cb57c4c122
akhil1975 <77316648+akhil1975@users.noreply.github.com>
2025-09-23 04:53:41 +0100

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -2,3 +2,11 @@ def sum(a,b)
 {
   echo " The sum of the two no ${a} and ${b} is ${a+b} "
 }
+def mutiply(a,b)
+{
+  echo " The mutiply of the two no ${a} and ${b} is ${a*b} "
+}
+def subtract(a,b)
+{
+  echo " The subtraction of the two no ${a} and ${b} is ${a-b} "
+}
